030a6c0ad86cf61bd83b826edeecb7965224091d
MaitaneSalazar <maitane3d@gmail.com>
2025-11-06 13:36:28 +0100

    Generar el numero del bombo y marcar el numero

--- BomboBingo/src/Bombo.java
+++ BomboBingo/src/Bombo.java
@@ -22,6 +22,12 @@ public class Bombo extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JButton newnumber;
+	private int[] arrayNumeros;
+	private JButton[] botones;
+	private JLabel nuevoNumlabel;
+	private JLabel antNumlabel;
+	private int cont=0;
 
 	/**
 	 * Launch the application.
@@ -52,7 +58,7 @@ public class Bombo extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel numeros = new JPanel();
 		numeros.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		numeros.setBackground(new Color(192, 192, 192));
@@ -60,67 +66,107 @@ public class Bombo extends JFrame {
 		contentPane.add(numeros);
 		numeros.setLayout(new GridLayout(9, 10, 0, 0));
 		
-		for (int i = 1; i <= 90; i++) {
-			JButton btn = new JButton(String.valueOf(i));
-			numeros.add(btn);
+		botones = new JButton[90];
+		for (int i = 0; i < 90; i++) {  // De 0 a 89
+		    JButton btn = new JButton(String.valueOf(i + 1));  // Texto del 1 al 90
+		    botones[i] = btn;  // Guardamos en posición i
+		    numeros.add(btn);
 		}
-		
+
 		JPanel control = new JPanel();
 		control.setBackground(new Color(192, 192, 192));
 		control.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		control.setBounds(521, 24, 351, 426);
 		contentPane.add(control);
 		control.setLayout(null);
-		
-		JButton newnumber = new JButton("Nuevo número");
-		newnumber.setFont(new Font("Tahoma", Font.BOLD, 12));
+
+		newnumber = new JButton("Nuevo número");
+		newnumber.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		newnumber.setBounds(62, 298, 147, 45);
 		control.add(newnumber);
-		
-		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setBounds(10, 37, 250, 250);
-		control.add(layeredPane);
-		
-		JLabel label = new JLabel("90");
-		label.setFont(new Font("Verdana", Font.BOLD, 50));
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		layeredPane.setLayer(label, 1);
-		label.setBounds(0, 0, 250, 239);
-		layeredPane.add(label);
-		label.setForeground(new Color(0, 0, 0));
-		
+
+		JLayeredPane NuevaPane = new JLayeredPane();
+		NuevaPane.setBounds(10, 37, 250, 250);
+		control.add(NuevaPane);
+
+		nuevoNumlabel = new JLabel("90");
+		nuevoNumlabel.setFont(new Font("Verdana", Font.BOLD, 50));
+		nuevoNumlabel.setHorizontalAlignment(SwingConstants.CENTER);
+		NuevaPane.setLayer(nuevoNumlabel, 1);
+		nuevoNumlabel.setBounds(0, 0, 250, 239);
+		NuevaPane.add(nuevoNumlabel);
+		nuevoNumlabel.setForeground(new Color(0, 0, 0));
+
 		JLabel BolaNueva = new JLabel("");
 		BolaNueva.setIcon(new ImageIcon("C:\\Users\\Dam1\\Documents\\DAM1\\Retos\\1Ev\\Reto1Java\\images\\bolaverde.png"));
 		BolaNueva.setBounds(0, 0, 251, 250);
-		layeredPane.add(BolaNueva);
-		
-		JLayeredPane layeredPane_1 = new JLayeredPane();
-		layeredPane_1.setBounds(241, 315, 100, 100);
-		control.add(layeredPane_1);
-		
-		JLabel label_1 = new JLabel("90");
-		layeredPane_1.setLayer(label_1, 1);
-		label_1.setHorizontalAlignment(SwingConstants.CENTER);
-		label_1.setForeground(Color.BLACK);
-		label_1.setFont(new Font("Verdana", Font.BOLD, 22));
-		label_1.setBounds(0, 0, 100, 95);
-		layeredPane_1.add(label_1);
-		
+		NuevaPane.add(BolaNueva);
+
+		JLayeredPane AnteriorPane = new JLayeredPane();
+		AnteriorPane.setBounds(241, 315, 100, 100);
+		control.add(AnteriorPane);
+
+		antNumlabel = new JLabel("90");
+		AnteriorPane.setLayer(antNumlabel, 1);
+		antNumlabel.setHorizontalAlignment(SwingConstants.CENTER);
+		antNumlabel.setForeground(Color.BLACK);
+		antNumlabel.setFont(new Font("Verdana", Font.BOLD, 22));
+		antNumlabel.setBounds(0, 0, 100, 95);
+		AnteriorPane.add(antNumlabel);
+
 		JLabel BolaAnterior = new JLabel("");
 		BolaAnterior.setIcon(new ImageIcon("C:\\Users\\Dam1\\Documents\\DAM1\\Retos\\1Ev\\Reto1Java\\images\\bolamorada.png"));
 		BolaAnterior.setBounds(0, 0, 100, 100);
-		layeredPane_1.add(BolaAnterior);
-		
+		AnteriorPane.add(BolaAnterior);
+
 		JLabel lblNewLabel = new JLabel("Bola Nueva");
 		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 20));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(10, 11, 250, 24);
 		control.add(lblNewLabel);
-		
+
 		JLabel lblBolaAnterior = new JLabel("Bola Anterior");
 		lblBolaAnterior.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblBolaAnterior.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBolaAnterior.setBounds(241, 298, 100, 14);
 		control.add(lblBolaAnterior);
+
+		arrayNumeros = new int [90];
+		registrarEventos();
 	}
+
+	public void registrarEventos() {
+		newnumber.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				nuevoNumero(arrayNumeros);
+			}
+		});
+	}
+	
+//generar el numero nuevo que no este repetido
+	public void nuevoNumero(int[] arrayNumeros2) {
+		int num;
+		boolean repetido;
+
+			do {
+				num = (int)(Math.random() * 90) + 1; 
+				repetido = false;
+
+				for(int j=0; j<cont;j++) {
+					if(arrayNumeros2[j] == num) {
+						repetido = true;
+					}
+				}
+
+			} while(repetido==true); //se repite todo el procedimiento hasta que repetido no sea false
+			
+			arrayNumeros2[cont] = num; //se introduce aqui el numero al assay
+			System.out.print(arrayNumeros2[cont] + " ");
+			nuevoNumlabel.setText(Integer.toString(num));
+			botones[num-1].setEnabled(false);
+			cont++;
+		}
 }
+
